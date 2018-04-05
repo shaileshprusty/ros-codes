11dfb3764aa81115476c67a3368c5921b1794979
shaileshprusty <shailesh.prusty@gmail.com>
2018-04-06 02:21:01 +0530

    Create ROS msg and srv (v2)

---
+++ catkin_ws/build/beginner_tutorials/CMakeFiles/beginner_tutorials_generate_messages_nodejs.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/beginner_tutorials_generate_messages_nodejs"
+  "/home/scp/ros-codes/catkin_ws/devel/share/gennodejs/ros/beginner_tutorials/msg/Num.js"
+  "/home/scp/ros-codes/catkin_ws/devel/share/gennodejs/ros/beginner_tutorials/srv/AddTwoInts.js"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/beginner_tutorials_generate_messages_nodejs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
