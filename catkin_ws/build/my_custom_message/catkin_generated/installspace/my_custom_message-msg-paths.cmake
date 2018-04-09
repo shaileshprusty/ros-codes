b3ea5a0cb244946eb12ef16890fac4f432dac94a
shaileshprusty <shailesh.prusty@gmail.com>
2018-04-09 12:43:04 +0530

    Trying to make a custom message package

---
+++ catkin_ws/build/my_custom_message/catkin_generated/installspace/my_custom_message-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.installspace.in
+
+_prepend_path("${my_custom_message_DIR}/.." "msg" my_custom_message_MSG_INCLUDE_DIRS UNIQUE)
+set(my_custom_message_MSG_DEPENDENCIES std_msgs)
---
+++ catkin_ws/build/my_custom_message/catkin_generated/my_custom_message-msg-extras.cmake.develspace.in
@@ -0,0 +1,2 @@
+set(my_custom_message_MESSAGE_FILES "/home/scp/ros-codes/catkin_ws/src/my_custom_message/msg/Number.msg")
+set(my_custom_message_SERVICE_FILES "")
