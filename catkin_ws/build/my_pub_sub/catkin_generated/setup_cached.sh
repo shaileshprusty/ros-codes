738cdbefd7e1adb0d15b85277bb119d3419a52a4
shaileshprusty <shailesh.prusty@gmail.com>
2018-05-01 01:14:26 +0530

    Modified yet not working

--- catkin_ws/build/my_pub_sub/catkin_generated/setup_cached.sh
+++ catkin_ws/build/my_pub_sub/catkin_generated/setup_cached.sh
@@ -8,8 +8,6 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/scp/ros-codes/catkin_ws/devel/.private/my_pub_sub:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/home/scp/ros-codes/catkin_ws/devel/.private/my_pub_sub/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="/home/scp/ros-codes/catkin_ws/devel/.private/my_pub_sub/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/scp/ros-codes/catkin_ws/build/my_pub_sub"
 export ROSLISP_PACKAGE_DIRECTORIES="/home/scp/ros-codes/catkin_ws/devel/.private/my_pub_sub/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/scp/ros-codes/catkin_ws/src/my_pub_sub:$ROS_PACKAGE_PATH"
--- catkin_ws/src/client_srv_pub_sub/src/process_node.cpp
+++ catkin_ws/src/client_srv_pub_sub/src/process_node.cpp
@@ -2,16 +2,17 @@
 #include "my_custom_message/num_sum.h"
 #include "my_custom_message/numbers.h"
 
-ros::NodeHandle n;
-
-ros::Publisher srv_pub = n.advertise<my_custom_message::num_sum>("srv_topic2", 1000);
-
 void process_Callback(const my_custom_message::numbers::ConstPtr& Arguments)
 {
   //ROS_INFO("Addition of %I64 and %I64.\n", Arguments->a, Arguments->b);
+  ros::NodeHandle m;
+
+  ros::Publisher srv_pub = m.advertise<my_custom_message::num_sum>("srv_topic2", 1000);
+
   std::cout<<"Addition of "<<Arguments->a<<" and "<<Arguments->b<<".\nCalculating SUM.....\n";
   my_custom_message::num_sum Sum;
   Sum.sum = Arguments->a + Arguments->b;
+  
   srv_pub.publish(Sum);
   std::cout<<"Calculation Done, Sum = "<<Sum.sum;
 }
@@ -20,6 +21,7 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "process");
 
+  ros::NodeHandle n;
   ros::Subscriber srv_sub = n.subscribe("srv_topic1", 1000, process_Callback);
 
   ros::spin();
